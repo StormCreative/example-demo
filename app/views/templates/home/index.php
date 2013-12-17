cfca61a7f0f8f2462bbcfb0fc897502d009b6846
AlexShearcroft <alex@stormcreative.co.uk>
2013-12-17 16:08:07 +0000

    first commit

--- app/views/templates/home/index.php
+++ app/views/templates/home/index.php
@@ -1,6 +1,11 @@
 <!-- THIRD + TWO THIRDS -->
 <div class="grid__two-third">
-	<p class="example">two thirds</p>
+	<div class="example">
+		<h1>Example title</h1>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+	</div>
 </div>
 <div class="grid__third">
 	<p class="example">third</p>
